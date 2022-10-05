f85fd7f2f3fdbdf9a56254c73b7f66ee199f6c44
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-10-05 18:28:22 -0300

    Implemented H2Gateway

--- infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderH2Gateway.java
+++ infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderH2Gateway.java
@@ -2,43 +2,53 @@ package br.com.cleanarch.infra.orders.persistence;
 
 import br.com.cleanarch.domain.orders.Order;
 import br.com.cleanarch.domain.orders.OrderGateway;
+import br.com.cleanarch.infra.orders.persistence.jpa.OrderEntityJpa;
+import br.com.cleanarch.infra.orders.persistence.jpa.OrderRepositoryJpa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Primary
 @Repository
 public class OrderH2Gateway implements OrderGateway {
 
+    @Autowired
+    private OrderRepositoryJpa orderRepository;
+
     @Override
     public Order create(Order order) {
-        return null;
+        OrderEntityJpa orderEntity = orderRepository.save(OrderEntityJpa.from(order));
+        return OrderEntityJpa.toDomain(orderEntity);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        orderRepository.deleteById(id);
     }
 
     @Override
     public Optional<Order> findById(Long id) {
-        return Optional.empty();
+        Optional<Order> order = orderRepository.findById(id).map(OrderEntityJpa::toDomain);
+        return order;
     }
 
     @Override
     public Order update(Order order) {
-        return null;
+        OrderEntityJpa orderEntity = orderRepository.save(OrderEntityJpa.from(order));
+        return OrderEntityJpa.toDomain(orderEntity);
     }
 
     @Override
     public List<Order> findAll() {
-        return null;
+        return orderRepository.findAll().stream().map(OrderEntityJpa::toDomain).collect(Collectors.toList());
     }
 
     @Override
     public boolean existsById(Long id) {
-        return false;
+        return orderRepository.existsById(id);
     }
 }
