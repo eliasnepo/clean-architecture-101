e46b5753d81d7dd24aa9e460beae3e89b66f5053
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-10-05 23:00:59 -0300

    Added entity manater implementation

--- infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderEntityManagerGateway.java
+++ infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderEntityManagerGateway.java
@@ -2,41 +2,76 @@ package br.com.cleanarch.infra.orders.persistence;
 
 import br.com.cleanarch.domain.orders.Order;
 import br.com.cleanarch.domain.orders.OrderGateway;
+import br.com.cleanarch.infra.orders.persistence.jpa.OrderEntityJpa;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class OrderEntityManagerGateway implements OrderGateway {
 
+    @Autowired
+    private EntityManager entityManager;
+
+    private final static Logger log = LoggerFactory.getLogger(OrderEntityManagerGateway.class);
+
     @Override
     public Order create(Order order) {
-        return null;
+        log.info("Inserting data by entity manager");
+        entityManager.persist(OrderEntityJpa.from(order));
+        return order;
     }
 
     @Override
     public void deleteById(Long id) {
-
+        log.info("Deleting data by entity manager");
+        entityManager.remove(entityManager.find(OrderEntityJpa.class, id));
     }
 
     @Override
     public Optional<Order> findById(Long id) {
-        return Optional.empty();
+        log.info("FindById data by entity manager");
+
+        OrderEntityJpa orderEntity = entityManager.find(OrderEntityJpa.class, id);
+
+        return Optional.of(OrderEntityJpa.toDomain(orderEntity));
     }
 
     @Override
     public Order update(Order order) {
-        return null;
+        log.info("Update data by entity manager");
+        OrderEntityJpa orderEntity = entityManager.merge(OrderEntityJpa.from(order));
+
+        return OrderEntityJpa.toDomain(orderEntity);
     }
 
     @Override
     public List<Order> findAll() {
-        return null;
+        log.info("Finding all data by entity manager");
+
+        String jpql = "SELECT o FROM OrderEntityJpa o";
+        TypedQuery<OrderEntityJpa> query = entityManager.createQuery(jpql, OrderEntityJpa.class);
+
+        return query.getResultList().stream().map(OrderEntityJpa::toDomain).collect(Collectors.toList());
     }
 
     @Override
     public boolean existsById(Long id) {
+        log.info("Exists data by entity manager");
+
+        OrderEntityJpa orderEntityJpa = entityManager.find(OrderEntityJpa.class, id);
+
+        if (orderEntityJpa != null) {
+            return true;
+        }
+
         return false;
     }
 }
--- infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderH2Gateway.java
+++ infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderH2Gateway.java
@@ -4,6 +4,8 @@ import br.com.cleanarch.domain.orders.Order;
 import br.com.cleanarch.domain.orders.OrderGateway;
 import br.com.cleanarch.infra.orders.persistence.jpa.OrderEntityJpa;
 import br.com.cleanarch.infra.orders.persistence.jpa.OrderRepositoryJpa;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
@@ -19,36 +21,44 @@ public class OrderH2Gateway implements OrderGateway {
     @Autowired
     private OrderRepositoryJpa orderRepository;
 
+    private final static Logger log = LoggerFactory.getLogger(OrderH2Gateway.class);
+
     @Override
     public Order create(Order order) {
+        log.info("Inserting data from JpaRepository");
         OrderEntityJpa orderEntity = orderRepository.save(OrderEntityJpa.from(order));
         return OrderEntityJpa.toDomain(orderEntity);
     }
 
     @Override
     public void deleteById(Long id) {
+        log.info("Deleting data from JpaRepository");
         orderRepository.deleteById(id);
     }
 
     @Override
     public Optional<Order> findById(Long id) {
+        log.info("FindById data from JpaRepository");
         Optional<Order> order = orderRepository.findById(id).map(OrderEntityJpa::toDomain);
         return order;
     }
 
     @Override
     public Order update(Order order) {
+        log.info("Updating data from JpaRepository");
         OrderEntityJpa orderEntity = orderRepository.save(OrderEntityJpa.from(order));
         return OrderEntityJpa.toDomain(orderEntity);
     }
 
     @Override
     public List<Order> findAll() {
+        log.info("Listing all data from JpaRepository");
         return orderRepository.findAll().stream().map(OrderEntityJpa::toDomain).collect(Collectors.toList());
     }
 
     @Override
     public boolean existsById(Long id) {
+        log.info("Exists data from JpaRepository");
         return orderRepository.existsById(id);
     }
 }
