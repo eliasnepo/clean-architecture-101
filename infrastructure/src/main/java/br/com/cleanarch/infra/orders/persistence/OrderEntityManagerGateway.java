b23298706e92c145cd2acdf8ddec5b552c4b270a
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-10-05 23:16:27 -0300

    Made some adjustments into entity manager

--- infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderEntityManagerGateway.java
+++ infrastructure/src/main/java/br/com/cleanarch/infra/orders/persistence/OrderEntityManagerGateway.java
@@ -6,14 +6,17 @@ import br.com.cleanarch.infra.orders.persistence.jpa.OrderEntityJpa;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Primary
 @Repository
 public class OrderEntityManagerGateway implements OrderGateway {
 
@@ -23,13 +26,19 @@ public class OrderEntityManagerGateway implements OrderGateway {
     private final static Logger log = LoggerFactory.getLogger(OrderEntityManagerGateway.class);
 
     @Override
+    @Transactional
     public Order create(Order order) {
         log.info("Inserting data by entity manager");
-        entityManager.persist(OrderEntityJpa.from(order));
-        return order;
+
+        OrderEntityJpa orderEntity = OrderEntityJpa.from(order);
+        entityManager.persist(orderEntity);
+        entityManager.flush();
+
+        return OrderEntityJpa.toDomain(orderEntity);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         log.info("Deleting data by entity manager");
         entityManager.remove(entityManager.find(OrderEntityJpa.class, id));
@@ -45,6 +54,7 @@ public class OrderEntityManagerGateway implements OrderGateway {
     }
 
     @Override
+    @Transactional
     public Order update(Order order) {
         log.info("Update data by entity manager");
         OrderEntityJpa orderEntity = entityManager.merge(OrderEntityJpa.from(order));
